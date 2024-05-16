5b16730a0fc7dc0802bfbbbba78318f82fe54351
oliviavitale35 <ov35@cornell.edu>
2024-05-16 13:24:21 -0400

    edited sheep.py so that it can generate individualized coeffs for each body in an array!

--- hydro/sheep.py
+++ hydro/sheep.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 farm = True
 rad = True               # rad only false for breakwater case
 
-w = np.array([0.5,0.65,0.75,0.85,0.95,1.047])   # wave frequency
+w = np.array([0.5,0.65,0.75,0.85,0.95,1.047,1.2])   # wave frequency
 B = 0                                           # wave direction [rad]
 depth = 40                                      # average water depth at southfork
 xtrans = np.array([0,0])
@@ -25,33 +25,63 @@ ytrans = np.array([50,-50])
 N = 3                           # number of bodies
 D = 30                          # distance btwn bodies
 ##################################################
+if farm == False:
+    Kr_H = []
+    Kt_H = []
+    Kr_K = []
+    Kt_K = []
+else:
+    Kr_H = [[] for _ in range(3)]
+    Kt_H = [[] for _ in range(3)]
+    Kr_K = [[] for _ in range(3)]
+    Kt_K = [[] for _ in range(3)]
 
-Kr_H = []
-Kt_H = []
-Kr_K = []
-Kt_K = []
 w_vals = []
 for w in w:
     if w < 0.8:
         res = 2
     else: 
-        res = 2                                   # resolution factor of grid wrt lambda
-    array = body.OSWEC(xtrans,ytrans,farm)
-    kd, total, incoming_fse, lam = solve.solver(array,B,depth,w,res,farm,rad)
-    #kd, total, incoming_fse, lam = attenuators.lpf(w,res,N,D,farm)
-    ref_H, trans_H, EB1, EB2 = wave_height.wave_height(total, incoming_fse,lam, res)
+        res = 3                                   # resolution factor of grid wrt lambda
+    #array = body.OSWEC(xtrans,ytrans,farm)
+    #kd, total, incoming_fse, lam = solve.solver(array,B,depth,w,res,farm,rad)
+    kd, total, incoming_fse, lam = attenuators.lpf(w,res,N,D,farm)
+    ref_H, trans_H, EB1, EB2 = wave_height.wave_height(total, incoming_fse,lam, res,xtrans,ytrans,farm)
     ref_K, trans_K, EB1, EB2 = kd_post.disturbance(kd, lam, res)
-    Kr_H.append(ref_H)
-    Kt_H.append(trans_H)
-    Kr_K.append(ref_K)
-    Kt_K.append(trans_K)
-    w_vals.append(w)
+    if farm == False:
+        Kr_H.append(ref_H)
+        Kt_H.append(trans_H)
+        Kr_K.append(ref_K)
+        Kt_K.append(trans_K)
+        w_vals.append(w)
+    else:
+        for i in range(3):
+            Kr_H[i].append(ref_H[i])
+            Kt_H[i].append(trans_H[i])
+            #Kr_K[i].append(ref_K[i])
+            #Kt_K[i].append(trans_K[i])
+        w_vals.append(w)
 
-plt.plot(w_vals,Kt_H,color='blue',marker='o',label='Kt from H')
-plt.plot(w_vals,Kr_H,color='red',marker='o',label='Kr from H')
-plt.plot(w_vals,Kt_K,color='blue',marker='o',linestyle=':',label='Kt from Kd')
-plt.plot(w_vals,Kr_K,color='red',marker='o',linestyle=':',label='Kr from Kd')
-plt.legend()
-plt.xlabel('w [rad/s]')
-plt.ylabel('Coefficient Value [m]')
-plt.show()
+if farm==False:
+    plt.plot(w_vals, Kt_H, color='blue', marker='o', label='$K_t$ from H')
+    plt.plot(w_vals, Kr_H, color='red', marker='o', label='$K_r$ from H')
+    plt.plot(w_vals, Kt_K, color='blue', marker='o', linestyle=':', label='$K_t$ from $K_d$')
+    plt.plot(w_vals, Kr_K, color='red', marker='o', linestyle=':', label='$K_r$ from $K_d$')
+    plt.legend()
+    plt.xlabel('$\omega$ [rad/s]')
+    plt.ylabel('Coefficient Value')
+    plt.savefig('/mnt/c/Users/ov162/transmission-reflection/hydro/figures/atten_coeffs.pdf')
+    plt.show()
+else:
+    linestyles = ['-', '--', ':', '-.', '+', 'x', '*']
+    for i, kt_h_values in enumerate(Kt_H):
+        plt.plot(w_vals, kt_h_values, marker='o', label=f'$K_t$ for body {i+1}', 
+                 linestyle=linestyles[i % len(linestyles)])
+    for i, kr_h_values in enumerate(Kr_H):
+        plt.plot(w_vals, kr_h_values, marker='o', label=f'$K_r$ for body {i+1}', 
+                 linestyle=linestyles[i % len(linestyles)])
+
+    plt.legend()
+    plt.xlabel('$\omega$ [rad/s]')
+    plt.ylabel('Coefficient Value')
+    plt.savefig('/mnt/c/Users/ov162/transmission-reflection/hydro/figures/atten_array_coeff.pdf')
+    plt.show()
--- hydro/wave_height.py
+++ hydro/wave_height.py
@@ -1,4 +1,4 @@
-def wave_height(total, incoming_fse,lam,res):
+def wave_height(total, incoming_fse,lam,res,xtrans,ytrans,farm):
     import numpy as np
     from scipy.signal import find_peaks
     import pandas as pd
@@ -6,19 +6,29 @@ def wave_height(total, incoming_fse,lam,res):
     warnings.filterwarnings("ignore", category=np.ComplexWarning)
     ##################################################################
     # Extract relevant columns
-    z_up = total[int((res/2)*lam-1),:int((res/2)*lam)]
     zinc_up = incoming_fse[int((res/2)*lam-1),:int((res/2)*lam)]
-    z_down = total[int((res/2)*lam-1),int((res/2)*lam):]
     zinc_down = incoming_fse[int((res/2)*lam-1),int((res/2)*lam):]
+    z_up = total[int((res/2)*lam-1),:int((res/2)*lam)]
+    z_down = total[int((res/2)*lam-1),int((res/2)*lam):]
 
     avg_H_zup = np.mean(abs(z_up))
     avg_H_zincup = np.mean(abs(zinc_up))
     avg_H_zdown = np.mean(abs(z_down))
     avg_H_zincdown = np.mean(abs(zinc_down))
 
+    if farm == True:
+        z_upWEC1 = total[int((res/2)*lam-1) + ytrans[0],:int((res/2)*lam)]
+        z_upWEC3 = total[int((res/2)*lam-1) + ytrans[1],:int((res/2)*lam)]
+
+        z_downWEC1 = total[int((res/2)*lam-1) + ytrans[0],int((res/2)*lam):]
+        z_downWEC3 = total[int((res/2)*lam-1) + ytrans[1],int((res/2)*lam):]
+
+        avg_H_zup = np.array([np.mean(abs(z_upWEC1)),np.mean(abs(z_up)),np.mean(abs(z_upWEC3))])
+        avg_H_zdown = np.array([np.mean(abs(z_downWEC1)),np.mean(abs(z_down)),np.mean(abs(z_downWEC3))])
+
     ref_H = abs(avg_H_zup/avg_H_zincup) - 1
     trans_H = abs(avg_H_zdown/avg_H_zincdown)
-    EB1 = trans_H**2 + ref_H**2     # energy balnce
+    EB1 = trans_H**2 + ref_H**2     # energy balance
     EB2 = (trans_H + ref_H)**2      # energy balance
     
     return ref_H, trans_H, EB1, EB2
