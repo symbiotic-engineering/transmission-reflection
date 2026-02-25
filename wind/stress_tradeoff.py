0abc522372ffbee11ef8d08844f021d1ff0e91dd
olviavitale35 <ov35@cornell.edu>
2026-02-25 14:20:59 -0500

    fixed some final errors, have solid results for cycles till failure from wave loads for different monopile sizes wrt wave height reduction

--- wind/stress_tradeoff.py
+++ wind/stress_tradeoff.py
@@ -16,8 +16,8 @@ rho_steel = 7980    # density of 316 stainless steel [kg/m^3]
 
 # parameters that could change
 # XL turbine dimensions (range for monopiles at depths > 30 m)
-d_XL = np.linspace(7,10,5)                # diameter of XL monopiles [m]
-t_XL = np.linspace(0.070,0.110,5)         # corresponding thickness of XL monopiles [m]
+d_XL = np.linspace(7,11,5)                # diameter of XL monopiles [m]                 (7,10)
+t_XL = np.linspace(0.070,0.125,5)         # corresponding thickness of XL monopiles [m] (0.070, 0.110)
 
 R = d_XL / 2             # outer radius [m]
 thickness = t_XL         # monopile wall thickness [m]
@@ -32,15 +32,16 @@ I = ((mass * (R**2 + r**2)) / 4) + ((mass * L**2) / 12)  # moment of inertia abo
 y = R                                                    # distance to centroidal axis
 
 T = 2 * np.pi / w                                        # wave period [s]
-H_values = np.linspace(0.900,0.999,3) * H_base           # reduced wave heights, percentages chosen as examples 
+H_values = np.linspace(0.700,0.999,30) * H_base           # reduced wave heights, percentages chosen as examples 
 percent_reduction_H = (1 - H_values / H_base) * 100      # percent reduction in H
 
 def calculate_damage(H,I,y,d):
+    print('wave height',H)
     def calculate_bending_stress(t,I,y,d):
         u = ((H * w) / 2) * (np.cosh(k * (z + h)) / np.sinh(k * h)) * np.cos(k * x - w * t)         # flow velocity
         u_dot = -((H * w**2) / 2) * (np.cosh(k * (z + h)) / np.sinh(k * h)) * np.sin(k * x - w * t) # flow acceleration
         f = C_m * rho * (np.pi / 4) * d**2 * u_dot + C_d * d * 0.5 * rho * u * np.abs(u)            # force of wave per unit length [N/m]
-        bending_moment = f * (L + H / 2)                                                            # L + H/2 is the moment arm [N-m] or [kg-m^2/s^2]
+        bending_moment = f * (L + H / 2) * H                                                        # L + H/2 is the moment arm, mutliply by wave height to get unit length of cylinder force acts upon [N-m] or [kg-m^2/s^2]
         sigma = (y / I) * bending_moment                                                            # bending stress (Pa) [m/s^2]
         return sigma
 
@@ -52,25 +53,26 @@ def calculate_damage(H,I,y,d):
     sigma_max = np.max(sigma_values)                    # this value is the amplitude of the sinusoidal stress curve
     sigma_min = np.min(sigma_values)                    # this is also the amplitude, but the negative value
     sigma_range = sigma_max - sigma_min                 # when you take this difference, you are effectively doubling the amplitude to get the sine wave height
-
+    print('sigma rnge',sigma_range)
     # a_bar value taken from experiments
     # for N > 10^6, m = 5
     m = 5
     a_bar = np.exp(13.617) 
     N_f = a_bar * (sigma_range**(-m))
+    print('nf',N_f)
 
     n_cycle = (25 * 8760 * 60 * 60) / T     # number of cycles in turbine lifetime (25 yrs)
     D = n_cycle / N_f
-    return D
+    return D, N_f
 
 # Calculate baseline damage at H_base
 percent_diff = [[],[],[],[],[]]
 for i in range(np.size(R)):
-    D_baseloop = calculate_damage(H_base,I[i],y[i],d[i])
+    D_baseloop, N_f = calculate_damage(H_base,I[i],y[i],d[i])
     percent_diffinter = []
     for H in H_values:
-        D_newloop = calculate_damage(H,I[i],y[i],d[i])
-        percent_diffloop = 100 * ((D_newloop - D_baseloop)/ D_baseloop)
+        D_newloop, N_floop = calculate_damage(H,I[i],y[i],d[i])
+        percent_diffloop = N_floop#100 * ((D_newloop - D_baseloop)/ D_baseloop)
 
         percent_diffinter.append(percent_diffloop)
     percent_diff[i] = percent_diffinter
@@ -78,16 +80,18 @@ for i in range(np.size(R)):
 print('percent diff',percent_diff)
 
 # Plotting
-plt.figure(figsize=(8, 6))
-plt.plot(percent_reduction_H, percent_diff[0], marker='o',color='#D55E00',markersize=10)
-plt.plot(percent_reduction_H, percent_diff[1], marker='>',markersize=10)
-plt.plot(percent_reduction_H, percent_diff[2], marker='+',markersize=10)
-plt.plot(percent_reduction_H, percent_diff[3], marker='x',markersize=10)
-plt.plot(percent_reduction_H, percent_diff[4], marker='*',markersize=10)
-plt.xlabel('Reduction of Wave Height [%]',fontsize=20)
-plt.ylabel('Reduction in Fatigue Damage [%]',fontsize=20)
-plt.xticks(fontsize=20)
-plt.yticks(fontsize=20)
+plt.figure(figsize=(10, 8))
+plt.plot(percent_reduction_H, percent_diff[4], marker='o',color='#D55E00',markersize=12)
+plt.plot(percent_reduction_H, percent_diff[3], marker='>',color='#E69F00',markersize=12)
+plt.plot(percent_reduction_H, percent_diff[2], marker='+',color='#56B4E9',markersize=12)
+plt.plot(percent_reduction_H, percent_diff[1], marker='x',color='#009E73',markersize=12)
+plt.plot(percent_reduction_H, percent_diff[0], marker='*',color='#0072B2',markersize=12)
+plt.legend(['d = 7m','d = 8m','d = 9m','d = 10m','d = 11m'],fontsize=20)
+plt.yscale('log')
+plt.xlabel('Reduction of Wave Height [%]',fontsize=24)
+plt.ylabel('Cycles to Failure [-]',fontsize=24)
+plt.xticks(fontsize=22)
+plt.yticks(fontsize=22)
 plt.grid(True)
 plt.tight_layout()
-plt.savefig('damage_reduction.pdf')
+plt.savefig('cycles_to_fail.pdf')
